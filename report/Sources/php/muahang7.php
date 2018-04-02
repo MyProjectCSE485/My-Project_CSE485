cd9dacbe0f047c97e336fab481d6b0dfcf8a82e4
nam1105 <36040534+nam1105@users.noreply.github.com>
2018-04-02 07:54:15 +0700

    update lai project

---
+++ report/Sources/php/muahang7.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Đặt Hàng7</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div id="wrap">
+		<header class="clear">
+			<div class="container">
+				<img class="img-res" src="img/image-1.png" alt="">
+			</div>
+		</header>
+		<nav>
+			<div class="container">
+				<ul class="main-menu clear">
+					<li><a href="index.php">TRANG CHỦ</a></li>
+					<li><a href="#">GIỚI THIỆU</a></li>
+					<li><a href="#">GÓC KĨ THUẬT</a></li>
+					<li><a href="#">TIN TỨC</a></li>
+					<li><a href="#">LIÊN HỆ</a></li>
+				</ul>
+				<div class="search navbar-right">
+					<form action="" method="POST" class="form-inline" role="form">
+						<div class="form-group">
+							<label class="sr-only" for="">label</label>
+							<input type="email" class="form-control" id="" placeholder="Tìm kiếm...">
+						</div>
+						<button type="submit" class="btn btn-primary">
+							<i class="glyphicon glyphicon-search"></i>
+						</button>
+					</form>	
+				</div>
+			</div>
+		</nav>
+		<main>
+			<div class="container">
+				<aside>
+					<div class="wrap-content">
+						<div class="title">
+							<h3>Giỏ hàng</h3>
+						</div>
+						<div class="giohang">
+							<form action="">
+								<div class="form-group clear">
+									<label class="label-control" for="">&nbsp</label>
+									<div class="control">
+										<input class="form-control" type="number" name="giohang" placeholder="sản phẩm">
+									</div>
+								</div>
+							</form>
+						</div>
+
+						<div class="title">
+							<h3>
+								Danh mục sản phẩm
+							</h3>
+						</div>
+
+						<div class="widget-content">
+							<ul class="menu-sidebar clear">
+								<li><a href="#">SAMSUNG</a>
+									<ul class="sub-menu">
+										<li><a href="#">SAMSUNG J7 PRO</a></li>
+										<li><a href="#">SAMSUNG J7 PRIME</a></li>
+										<li><a href="#">SAMSUNG S8</a></li>
+										<li><a href="#">SAMSUNG S8 PLUS</a></li>
+										<li><a href="#">SAMSUNG NOTE FE</a></li>
+										<li><a href="#">SAMSUNG NOTE 8</a></li>
+									</ul>
+								</li>
+								<li><a href="#">IPHONE</a>
+									<ul class="sub-menu">
+										<li><a href="#">IPHONE X</a></li>
+										<li><a href="#">IPHONE 8</a></li>
+										<li><a href="#">IPHONE 8 PLUS</a></li>
+										<li><a href="#">IPHONE 7</a></li>
+										<li><a href="#">IPHONE 7 PLUS</a></li>
+										<li><a href="#">IPHONE 6S PIUS</a></li>
+									</ul>
+								</li>
+								<li><a href="#">LINH PHỤ KIỆN</a>
+									<ul class="sub-menu">
+										<li><a href="#">MÀN HÌNH SAMSUNG</a></li>
+									</ul>
+								</li>
+								<li><a href="#">SỬA CHỮA</a></li>
+								<li><a href="#">HÀNG KHUYẾN MÃI</a>
+									<ul class="sub-menu">
+										<li><a href="#">LAPTOP DELL</a></li>
+										<li><a href="#">LAPTOP HP</a></li>
+										<li><a href="#">BALO LAPTOP</a></li>
+										<li><a href="#">TÚI CHỐNG SỐC LAPTOP</a></li>
+									</ul>
+								</li>
+								<li><a href="#">TIN CÔNG NGHỆ</a></li>
+								<li><a href="#">KHO MÁY TÍNH</a></li>
+							</ul>
+						</div>
+
+						<div class="title">
+							<h3>Hỗ trợ trực tuyến</h3>
+						</div>
+
+						<div class="widget-content">
+							<ul class="menu-sidebar">
+								<li>Hotline:0939 686 111</li>
+								<li>SAMSUNG MINHSIEU</li>
+							</ul>
+						</div>
+
+
+
+						<div class="title">
+							<h3>Liên kết webside</h3>
+						</div>
+
+						<div class="lienket">
+							<form action="">
+								<div class="form-group clear">
+									<label class="label-control" for="">&nbsp</label>
+									<div class="control">
+										<select class="form-control" name="" id="">
+											<option value="">---Chọn liên kết---</option>
+											<option value="1">Thiết bị tưới</option>
+											<option value="2">Báo tuổi trẻ</option>
+											<option value="3">Báo thanh niên</option>
+											<option value="4">Báo công nghệ</option>
+											<option value="5">Báo bóng đá</option>
+										</select>
+									</div>
+								</div>
+							</form>
+						</div>
+
+						<div class="ads">
+							<a href="">
+								<img class="img-res" src="img/img-2.jpg" alt="">
+							</a>
+						</div>
+						<div class="ads">
+							<a href="">
+								<img class="img-res" src="img/img-3.jpg" alt="">
+							</a>
+						</div>
+					</div>
+				</aside>
+				<section>
+					<div class="wrap-content">
+						
+
+					<div class="row">
+						<table class="table table-hover">
+							<thead>
+								<tr>
+									<th>Sản Phẩm</th>
+									<th>Số Lượng</th>
+									<th>Đơn Giá(VNĐ)</th>
+									<th>Thành Tiền(VNĐ)</th>
+									<th>Xóa</th>
+									<th>Còn Hàng</th>
+									<th>Thanh Toán</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>IPHONE 6S PLUS</td>
+									<td>
+										<form class="form-table" action="" <!-- style="width: 100px" -->
+								            <div class="form-group clear">
+									        <label class="label-control" for="">&nbsp</label>
+									        <div class="control">
+										    <input class="form-control" type="number" name="giohang" >
+									        </div>
+								</div>
+							</form>
+									</td>
+									<td>24.000.000VNĐ</td>
+									<td>24.000.000VNĐ</td>
+									<td>
+										<a href="#">XÓA</a>
+									</td>
+									<td>
+										<span class="label label-success">Còn Hàng</span>
+									</td>
+									<td>
+										<a href="formdathang.php">Thanh Toán</a>
+									</td>
+									
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</section>
+			</div>
+			<div id="gotop">
+			    <a href="#">
+			    	<img onclick="topFunction()"  id="myBtn" title="gototop" src="img/gotop.png" alt="">
+			    </a>
+		    </div>
+		</main>
+		<footer>
+			
+				<p>Địa chỉ:16 Nguyễn Khánh Toàn, TP Hà Nội</p>
+				<p>Tell:0984435480</p>
+				<p>Email:samsungminhsieu@gmail.com</p>
+				<i class="fa fa-angle-left"></i>
+		</footer>
+
+	</div>
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/app.js"></script>
+</body>
+</html>
---
+++ report/Sources/php/product_list.php
@@ -0,0 +1,39 @@
+<!-- <?php
+
+$arrSanPham = $db->getsanpham();
+					//var_dump($menus);
+					foreach ($arrSanPham as $sanpham){
+						?>
+						
+
+
+
+<div class="row">
+				        <div class="col-md-4">
+					        <div class="item-product">
+						        <div class="img-product">
+
+						        	<a href="chitietsanpham1.php>" class="">
+						        		// <img  src="<?php echo $sanpham['HinhAnh']; ?>" alt="">
+						        	</a>
+
+						        </div>
+						        <div class="text-center">
+						            <h4 class="item-title"><a href="chitietsanpham1.php"><?php echo $sanpham['TenSanPham']; ?></a></h4>
+						            <p class="text-danger"><?php echo $sanpham['DonGia']; ?></p>
+						            <p class="text-center">
+						            	<button class="btn btn-success btn-sm">
+											<i class="glyphicon glyphicon-shopping-cart"></i> Mua ngay
+										</button>
+										<button class="btn btn-primary btn-sm">
+											<i class="glyphicon glyphicon-eye-open"></i>Chi tiết
+										</button>
+						            </p>
+					            </div>
+					        </div>      
+				        </div>
+</div>
+
+
+					    }
+					    ?> -->
